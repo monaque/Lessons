8254d39d72a3fef130d0665a0fbb27b8ed56718a
Valeriy <valeriy.drevko@gmail.com>
2021-08-29 21:02:39 +0400

    Второе дз

--- HomeWork2_3.cs
+++ HomeWork2_3.cs
@@ -8,5 +8,17 @@ namespace Lessons1.Lesson_2
 {
     class HomeWork2_3
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            var a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+                Console.WriteLine("Введенное число четное");
+
+            else
+                Console.WriteLine("Введенное число нечетное");
+
+        }
     }
 }
