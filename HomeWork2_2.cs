ec4e40e19ac5440ff556cacd5d9fc1511d3e03f9
Valeriy <valeriy.drevko@gmail.com>
2021-08-29 20:50:05 +0400

    Второе дз

---
+++ HomeWork2_2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Lessons1.Lesson_2
+{
+    class HomeWork2_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите порядковый номер месяца");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(((Months)a).ToString());
+        }
+
+        enum Months
+        {
+            Январь = 1,
+            Февраль = 2,
+            Март = 3,
+            Апрель = 4,
+            Май = 5,
+            Июнь = 6,
+            Июль = 7,
+            Август = 8,
+            Сентябрь = 9,
+            Октябрь = 10,
+            Ноябрь = 11,
+            Декабрь = 12,
+        }
+    }
+}
---
+++ HomeWork2_4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons1.Lesson_2
+{
+    class HomeWork2_4
+    {
+    }
+}
