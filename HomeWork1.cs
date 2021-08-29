fc3375bbf5aef351bff8537beaa247de2ac64c05
Valeriy <valeriy.drevko@gmail.com>
2021-08-29 20:53:06 +0400

    Первое дз

---
+++ HomeWork1.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+
+
+//namespace Lessons1.Lesson_1
+//{
+//    class HomeWork1
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Введите Ваше имя:");
+//            var name = Console.ReadLine();
+//            var date = DateTime.Now;
+//            Console.WriteLine($"Привет, {name}, сегодня {date} ");
+//        }
+//    }
+//}
