f835ca45b7717afdca5ad029e542a6625439c3ce
Valeriy <valeriy.drevko@gmail.com>
2021-09-05 21:03:44 +0400

    Третье дз

---
+++ HomeWork3_2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Lessons1.Lesson_3
+{
+    class HomeWork3_2
+    {
+        //static void Main(string[] args)
+        //{
+        //    string[,] array =
+        //    {
+        //        { "Дмитрий", "+79279853214" },
+        //        { "Людмила", "lyudmila1984@mail.ru" },
+        //        { "Сергей", "+79649918396" },
+        //        { "Арина", "arina@mail.ru" },
+        //        { "Валерий", "+79179497939" },
+        //    };
+
+        //    Console.WriteLine("Телефонный справочник:");
+        //    for (int i = 0; i < array.GetLength(0); i++)
+        //    {
+        //        Console.WriteLine($"{array[i, 0]}, {array[i, 1]}");
+        //    }
+        //}
+    }
+}
